70f8ff62b95161e39152ce89c5501321d7629c89
direyesm <37608963+direyesm@users.noreply.github.com>
2021-01-18 16:04:52 -0300

    bootstrap + popper + jquery

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -2,7 +2,19 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-personas',
-    templateUrl: './personas.component.html'
+    templateUrl: './personas.component.html',
+
+    // archivo por separados
+    styleUrls: ['./personas.component.css']
+    
+
+    //Archivo directo
+    // styles: [`
+    // h1{
+    //     color:blue;
+    // }
+    // `]
+
 })
 export class PersonasComponent{
 
